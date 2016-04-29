0a355f4addaf8645883191325337f389e680e9fb
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-29 00:23:14 -0400

    Add Retrofit interceptor class

---
+++ app/src/main/java/com/example/junitsimple/RetrofitMagic.java
@@ -0,0 +1,65 @@
+package com.example.junitsimple;
+
+import com.example.junitsimple.entities.Repository;
+
+import java.io.IOException;
+import java.util.List;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+/**
+ * Created by evin on 4/28/16.
+ */
+
+public class RetrofitMagic {
+    private static final String BASE_URL = "https://api.github.com/";
+
+    public static void main(String... args) {
+        System.out.println(getRepositories("Evin1-"));
+    }
+
+    public static Retrofit buildRetrofit() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static List<Repository> getRepositories(String username) {
+        Retrofit retrofit = buildRetrofit();
+
+        GitHubService gitHubService = retrofit.create(GitHubService.class);
+
+        Call<List<Repository>> callRepositories = gitHubService.callRepositories(username);
+
+        List<Repository> listRepositories = null;
+
+        try {
+            listRepositories = callRepositories.execute().body();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return listRepositories;
+    }
+
+    //http://stackoverflow.com/questions/32514410/logging-with-retrofit-2
+    //https://api.github.com/users/Evin1-/repos
+
+    public interface GitHubService {
+        @GET("/users/{username}/repos")
+        Call<List<Repository>> callRepositories(@Path("username") String username);
+    }
+}

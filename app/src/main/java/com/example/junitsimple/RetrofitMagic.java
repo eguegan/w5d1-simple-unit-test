18cfcb50d9a279f0c0ffa6bc02e8592865f2c904
Evin1- <edwin.hdz.12.0@gmail.com>
2016-04-29 00:25:10 -0400

    Optimize imports

--- app/src/main/java/com/example/junitsimple/RetrofitMagic.java
+++ app/src/main/java/com/example/junitsimple/RetrofitMagic.java
@@ -9,7 +9,6 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 /**
